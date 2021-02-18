1f240bb4ac83b52a41bc886343789f2c7f26b670
Alex Gill <alex.gill@theuniprogroup.com>
2021-02-18 22:52:42 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Gulp with sass, JS, Babel, Lint, Minify and livereload
+
+**Assumptions:**
+
+- You already have a development environment
+- Node/NPM is installed
+
+## How to install
+
+```
+npm install
+```
+
+## How to run
+
+```
+# Install the gulp command line utility
+npm install --global gulp-cli
+
+# Run Gulp task(s)
+gulp
+
+```
+
+## LiveReload
+This is optional but you can install the [Chrome Extension](http://livereload.com/extensions/)
---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const rename = require('gulp-rename')
+const sassLint = require('gulp-sass-lint');
+const eslint = require('gulp-eslint7')
+const babel = require('gulp-babel')
+const minify = require('gulp-minify')
+const livereload = require('gulp-livereload')
+
+const sassPath = './assets/sass/**/*'
+const jsPath = './assets/js/**/*'
+
+function css () {
+  return gulp
+    .src(sassPath)
+    .pipe(sassLint({ configFile: '.sass-lint.yml' }))
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./dist'))
+    .pipe(livereload())
+}
+
+function scripts () {
+  return gulp
+    .src(jsPath)
+    .pipe(eslint())
+    .pipe(babel({ presets: ['@babel/env'] }))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+    .pipe(minify({ ext: { min: '.min.js' } }))
+    .pipe(gulp.dest('./dist'))
+    .pipe(livereload())
+}
+
+exports.default = function () {
+  livereload.listen()
+  gulp.watch(sassPath, css)
+  gulp.watch(jsPath, scripts)
+}
